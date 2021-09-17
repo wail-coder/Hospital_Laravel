31d6215d2caad6df4aa5253ae3b3ac3c5421d2ee
wail-coder <shopwking@gmail.com>
2021-09-17 20:47:57 +0300

    solve

--- database/migrations/2021_09_08_153644_create_notifications_table.php
+++ database/migrations/2021_09_08_153644_create_notifications_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateNotificationsTable extends Migration
 {
     /**
-     * Run the migrations.
+     * run the migrations.
      *
      * @return void
      */
